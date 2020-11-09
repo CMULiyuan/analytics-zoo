913d2dbf83aecf21912df63a97690686bc824af8
GavinGu07 <gavin7gu@gmail.com>
2020-11-09 17:04:43 +0800

    update test

--- zoo/src/test/scala/com/intel/analytics/zoo/pipeline/inference/test.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/pipeline/inference/test.scala
@@ -8,22 +8,23 @@ import com.intel.analytics.zoo.core.TFNetNative
 import com.intel.analytics.zoo.pipeline.api.keras.ZooSpecHelper
 import com.intel.analytics.zoo.pipeline.api.net.TorchModel
 import org.apache.log4j.{Level, Logger}
-
 import com.intel.analytics.zoo.common.NNContext.initNNContext
 import com.intel.analytics.zoo.pipeline.api.keras.layers.utils.EngineRef
 import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.FlatSpec
 
 import scala.language.postfixOps
 
-class test extends ZooSpecHelper with InferenceSupportive {
+class test extends FlatSpec {
   val model = new InferenceModel(4)
   "PyTorch Model" should "be loaded and do predict" in {
     model.doLoadPyTorch("/Users/liyuangu/Desktop/Intel/torch_resnet50/SimpleTorchModel.pt")
-    model.getOriginalModel shouldNot be(null)
+    assert(model != null)
     val inputTensor = Tensor[Float](1, 2).rand()
     val exceptedResult = inputTensor.valueAt(1, 1) * 0.2f +
       inputTensor.valueAt(1, 2) * 0.5f + 0.3f
     val r = model.doPredict(inputTensor)
-    r should be(Tensor[Float](Array(exceptedResult), Array(1, 1)))
+    assert(r == Tensor[Float](Array(exceptedResult), Array(1, 1)))
+    
   }
 }
